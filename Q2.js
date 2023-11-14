311eaeb70d05c411ddef47c36cb610a1c16c1709
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2023-11-14 12:46:10 +0200

    my recent update

---
+++ Q2.js
@@ -0,0 +1,5 @@
+
+function product(e,f){
+    return e*f;
+}
+console.log(4,3)
---
+++ Q7.js
@@ -0,0 +1,10 @@
+function smallest(arr){
+    var small = arr[0]
+    for(let i=1; i<arr.length;i++){
+        if(arr[i]<arr[i-1]){
+             small = arr[i];
+        }
+    }
+    return small;
+}
+console.log(smallest([1,2,3,4,5,]))
